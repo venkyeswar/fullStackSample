91f4ac598e6df1b532c418f78dcdffe1d3832a52
venkyeswar <venkyeswar2037@gmail.com>
2023-09-13 18:58:26 +0530

    initial commit

---
+++ data/database.js
@@ -0,0 +1,28 @@
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+let mongodbUrl="mongodb://0.0.0.0";
+
+
+if(process.env.MONGODB_URL){
+  mongodbUrl=process.env.MONGODB_URL;
+}
+let database;
+
+async function initDatabase() {
+  const client = await MongoClient.connect(mongodbUrl);
+  database = client.db('deployment');
+}
+
+function getDb() {
+  if (!database) {
+    throw new Error('No database connected!');
+  }
+
+  return database;
+}
+
+module.exports = {
+  initDatabase: initDatabase,
+  getDb: getDb,
+};
